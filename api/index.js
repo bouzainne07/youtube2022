64bcfbdad566acd4a0e4e58981966bf1217868c4
Amine <166140201+ampls-911@users.noreply.github.com>
2025-03-08 22:43:45 +0100

    init

---
+++ api/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+
+
+const app = express()
+dotenv.config()
+
+
+const connect = async () => {
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb")
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("Mongodb disconnected")
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("Mongodb connected")
+})
+
+app.get("/", (req,res)=>{
+    res.send("hello first req waywa")
+})
+
+app.listen (8800, ()=>{
+    connect ()
+    console.log("connected to backend")
+})
