35849a4921d09c1af3f38c18583b7e4a9962cba8
bouzainne07 <166277632+bouzainne07@users.noreply.github.com>
2025-04-20 00:06:25 +0100

    firas

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,8 +1,7 @@
-import User from "../models/User.js"
 import bcrypt from 'bcrypt';
-import { createError } from '../utils/error.js'; 
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv"
+import User from "../models/User.js";
+import { createError } from '../utils/error.js';
 
 
 
@@ -18,6 +17,7 @@ export const register = async (req,res,next) =>{
             username:req.body.username,
             email:req.body.email,
             password:hash,
+            isadmin:req.body.isadmin,
 
         })
 
@@ -30,12 +30,12 @@ export const register = async (req,res,next) =>{
 }
 export const login = async (req,res,next) =>{
     try{
-      const user = await User.findOne({
+    const user = await User.findOne({
         username:req.body.username
-      })
-      if (!user) return next(createError(404, "user not found"))
+    })
+    if (!user) return next(createError(404, "user not found"))
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-      if (!isPasswordCorrect) return next(createError(400, "banned request"))
+    if (!isPasswordCorrect) return next(createError(400, "banned request"))
         const token = jwt.sign({id:user._id,isadmin:user.isadmin}, process.env.jwt)
         const {password, isadmin, ...otherDetails} = user._doc;
         res.cookie("access_token",token, {
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,18 +1,28 @@
-import express from "express"
-import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from "../controllers/room.js";
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
-//create
-router.post("/:hotelid",verifyAdmin, createRoom)
-//update
-router.put("/:id",verifyAdmin, updateRoom)
-//delete
-router.delete("/:id/:hotelid",verifyAdmin, deleteRoom)
-//get
-router.get("/:id", getRoom)
-//get all
-router.get("/", getAllRooms)
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
 
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
 
-export default router
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
+
+export default router;
